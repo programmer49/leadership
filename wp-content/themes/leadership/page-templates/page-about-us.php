914e293ceb87edf6871fd0be30f5225050445949
philweb49 <philweb.programmer49@gmail.com>
2016-02-23 10:54:07 +0800

    updates

--- wp-content/themes/leadership/page-templates/page-about-us.php
+++ wp-content/themes/leadership/page-templates/page-about-us.php
@@ -7,122 +7,70 @@ while (have_posts()): the_post();
 
 <div class="about-us section">
 	<div class="container">
-		<h2 class="section-title">About the Leadership Group</h2>
-		<p class="header-text">We work with leaders, teams and organisations to transform the level of their effectiveness and performance.</p>
+		<h2 class="section-title"><?php the_title(); ?></h2>
+		<?php the_content(); ?>
 	</div>
 
 	<div class="what-we-do section section-l3 animatedParent animateOnce">
     <div class="content">
         <div class="content-left">
-	        <h2 class="section-title section-title-white animated fadeInUp delay-250 go">What we do</h2>
+	        <h2 class="section-title section-title-white animated fadeInUp delay-250 go"><?php the_field( 'what_we_do_title' ); ?></h2>
 	        <div class="description animated fadeInUp delay-500 go">
-	            <p>Whether the focus is on development as an organisation, team or individual, the core of our work is about raising levels of capability and consciousness.</p>
-
-	            <p>Leaders, teams and organisations need capabilities, which need to be identified and systematically developed.  And leaders need to be operating at a level of consciousness which enables them to make the right choices, whether that is choices about strategy, or behaviour.</p>
+	            <?php the_field( 'what_we_do_content' ); ?>
 	        </div>
 
 					<div class="row">
-						<div class="col-md-4">
+						<div class="col-md-12">
 							<ul>
-								<li>PERSONAL</li>
-								<li>EFFECTIVENESS</li>
-							</ul>
-						</div>
-
-						<div class="col-md-4">
-							<ul>
-								<li>ORGANIZATION</li>
-								<li>EFFECTIVENESS</li>
-							</ul>
-						</div>
-
-						<div class="col-md-4">
-							<ul>
-								<li>BUSINESS</li>
-								<li>PERFOMANCE</li>
+								<?php foreach( get_field( 'what_we_do_keyword' ) as $i => $p ):; ?>
+								<li><?php echo $p['title']; ?></li>
+								<?php endforeach; ?>
 							</ul>
 						</div>
 					</div>
 
     		</div>
-    		<div class="content-right" style="background-image: url('/wp-content/uploads/2016/02/leadership-group-img.jpg');"></div>
+    		<div class="content-right" style="background-image: url('<?php the_field( 'picture' ); ?>');"></div>
     </div>
 	</div>
 
 	<div class="container">
 		<div class="our-philosophy section">
-			<h2 class="section-title">Our Philosophy</h2>
-			<p><span>Our core belief as a team</span> is that we have to role-model what we do with our clients in order to <span>maintain the privileged position that we have with our clients.</span></p>
-
-			<div class="gap gap-20"></div>
-
-			<p>So, <span>all of the core members of our team are on their own leadership development journeys,</span> all do regular leadership 360s and receive coaching.  <span>We work on our team effectiveness on an ongoing basis,</span> and use the same approach to <span>building our organisation effectiveness,</span> and developing and executing strategy, that we use with our clients.</p>
-
-			<div class="gap gap-20"></div>
-
-			<p>By the same measures we use with our clients, <span>we are a high performing organisation and team, and you will experience this when you work with us.</span></p>
+			<h2 class="section-title"><?php the_field( 'our_philosophy_title' ); ?></h2>
+			<?php the_field( 'our_philosophy_content' ); ?>
 		</div>
 	</div>
 
 	<div class="the-leadership-team">
 		<div class="container">
-			<h2 class="section-title">The Leadership Team</h2>
-			<p class="header-text">The core of our team started working together over 20 years ago and we are held together by strong alignment on Mission Vision Values and Goals.</p>
-
+			<h2 class="section-title"><?php the_field( 'team_title' ); ?></h2>
+			<p class="header-text"><?php the_field( 'team_content' ); ?></p>
+			<?php $q = get_posts( array( 'post_type' => 'team', 'post__in' => get_field( 'members' ) ) ); ?>
 			<ul class="the-leadership-team-list">
 				<div class="row">
+				<?php foreach ( $q as $i => $p ): ?>
 					<li class="col-sm-6 col-md-4 the-leadership-team-list-item">
-	          <div class="our-team-link-holder">
-	              <div class="our-team-img-wrap" style="background:url(/wp-content/themes/leadership/assets/images/jeremy-img.png) no-repeat center center"></div>
-	              <p class="name">Jeremy Perks</p>
-	              <p class="position">Founder and Managing Director Leads the Organisation Effectiveness Practice</p>
-	          </div>
-	        </li>
+						<div class="our-team-link-holder">
+							<div class="our-team-img-wrap" style="background:url('<?php attachment_image_src( $p->ID, 'post-thumbnail' ); ?>') no-repeat center center"></div>
+							<p class="name"><?php echo $p->post_title; ?></p>
+							<p class="position"><?php the_field( 'position', $p->ID ); ?></p>
+						</div>
+	        		</li>
+	    		<?php endforeach; ?>
 
-	        <li class="col-sm-6 col-md-4 the-leadership-team-list-item">
-	          <div class="our-team-link-holder">
-	              <div class="our-team-img-wrap" style="background:url(/wp-content/themes/leadership/assets/images/jeremy-img.png) no-repeat center center"></div>
-	              <p class="name">Ray Wang</p>
-	              <p class="position">Senior Consultant & Partner Leads the Leadership Practice</p>
-	          </div>
-	        </li>
-
-	        <li class="col-sm-6 col-md-4 the-leadership-team-list-item">
-	          <div class="our-team-link-holder">
-	              <div class="our-team-img-wrap" style="background:url(/wp-content/themes/leadership/assets/images/mandy.png) no-repeat center center"></div>
-	              <p class="name">Mandy Zhang</p>
-	              <p class="position">COO & Partner Leads the Assessments Practice</p>
-	          </div>
-	        </li>
-
-	        <li class="col-sm-6 col-md-4 the-leadership-team-list-item">
-	          <div class="our-team-link-holder">
-	              <div class="our-team-img-wrap" style="background:url(/wp-content/themes/leadership/assets/images/mandy.png) no-repeat center center"></div>
-	              <p class="name">Tracy Driscoll</p>
-	              <p class="position">Senior Consultant & Master Coach Leads the Coaching Practice</p>
-	          </div>
-	        </li>
-
-	        <li class="col-sm-6 col-md-4 the-leadership-team-list-item">
-	          <div class="our-team-link-holder">
-	              <div class="our-team-img-wrap" style="background:url(/wp-content/themes/leadership/assets/images/mandy.png) no-repeat center center"></div>
-	              <p class="name">Juliet Chen</p>
-	              <p class="position">Talented consultant facilitator coach & program designer</p>
-	          </div>
-	        </li>
+	        
 
 	        <li class="col-sm-6 col-md-4 the-leadership-team-list-item">
 	          <div class="our-team-link-holder">
 	            <div class="consulting-team">
-	            	<h4 class="consulting-team-header">The strong back office includes</h4>
+	            	<h4 class="consulting-team-header"><?php the_field(  'last_box_title' ); ?></h4>
 		            <ul>
-		            	<li><strong>Sabrina Qi</strong> (Financial Controller)</li>
-		            	<li><strong>Sharon Shi</strong> (Project Management)</li>
-		            	<li><strong>Alice Wang</strong> (IT and Administration),</li>
-		            	<li><strong>Yan Yan</strong> (Accounting).</li>
+		            	<?php while ( have_rows( 'back_office_staff' ) ): the_row(); ?>
+		            	<li><strong><?php the_sub_field( 'name' ); ?></strong> (<?php the_sub_field( 'position' ); ?>)</li>
+		            	<?php endwhile; ?>
 		            </ul>
 
-		            <a href="#" class="btn btn-primary btn-orange consulting-team-link">View Consulting Team</a>
+		            <a href="<?php the_permalink( get_page_by_path( 'our-team' ) ); ?>" class="btn btn-primary btn-orange consulting-team-link"><?php the_field( 'last_box_button_text' ); ?></a>
 	            </div>
 	          </div>
 	        </li>
@@ -136,112 +84,71 @@ while (have_posts()): the_post();
 	<div class="our-clients section">
 		<div class="container">
 			
-			<h2 class="section-title">Some Clients</h2>
+			<h2 class="section-title"><?php the_field( 'client_title' ); ?></h2>
 
 			<ul class="nav nav-tabs clients-list">
-		    <li class="active col-sm-6 col-md-3">
-		    	<a data-toggle="tab" href="#home"><img src="/wp-content/themes/leadership/assets/images/coke-logo.png" class="grayscale" alt=""></a>
-		    </li>
-
-		    <li class="col-sm-6 col-md-3">
-		    	<a data-toggle="tab" href="#menu1"><img src="/wp-content/themes/leadership/assets/images/coke-logo.png" class="grayscale" alt=""></a>
-		    </li>
-
-		    <li class="col-sm-6 col-md-3">
-		    	<a data-toggle="tab" href="#menu2"><img src="/wp-content/themes/leadership/assets/images/coke-logo.png" class="grayscale" alt=""></a>
-		    </li>
-
-		    <li class="col-sm-6 col-md-3">
-		    	<a data-toggle="tab" href="#menu3"><img src="/wp-content/themes/leadership/assets/images/coke-logo.png" class="grayscale" alt=""></a>
-		    </li>
-		  </ul>
+				<?php foreach ( get_field( 'clients' ) as $i => $p ):?>
+			    <li class="<?php echo $i == 0 ? 'active' : ''; ?> col-sm-6 col-md-3">
+			    	<a data-toggle="tab" href="#client-<?php echo $i; ?>"><img src="<?php echo $p['logo']; ?>" class="grayscale" alt=""></a>
+			    </li>
+				<?php endforeach ?>
+		  	</ul>
 
 		  <div class="tab-content client-testimonial">
-		    <div id="home" class="tab-pane fade in active">
-		      <p>“The Leadership Group bring a uniquely insightful, highly situational approach to their work with clients. We have engaged them across major worldwide events such as the Olympic Games, complex organizational transformations, and also executive team interventions. In each case, the approach they have recommended was unique, carefully designed and extremely well implemented. We will definitely turn to them again in the future."</p>
+		  	<?php foreach ( get_field( 'clients' ) as $i => $p ): ?>
+		    <div id="client-<?php echo $i; ?>" class="tab-pane fade in <?php echo $i == 0 ? 'active' : ''; ?>">
+		      <p>“<?php echo $p['testimonial']; ?>"</p>
 		      <div class="testimonial-author">
-		      	<p>David G. Brooks <br>
-		      	Executive Vice President <br>
-		      	Coca-Cola Greater China & Korea</p>
+		      	<p><?php echo $p['name']; ?><br>
+		      	<?php echo $p['position']; ?> <br>
+		      	<?php echo $p['company']; ?></p>
 		      </div>
 		    </div>
-		    <div id="menu1" class="tab-pane fade">
-		      <h3>Menu 1</h3>
-		      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-		    </div>
-		    <div id="menu2" class="tab-pane fade">
-		      <h3>Menu 2</h3>
-		      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-		    </div>
-		    <div id="menu3" class="tab-pane fade">
-		      <h3>Menu 3</h3>
-		      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-		    </div>
+			<?php endforeach; ?>
 		  </div>
-			<a href="#" class="btn btn-primary btn-orange discover-offers">DISCOVER WHAT WE OFFER</a>
+			<a href="<?php the_permalink( get_page_by_path( 'our-services' ) ); ?>" class="btn btn-primary btn-orange discover-offers"><?php the_field( 'discover_button_text' ); ?></a>
 		</div>
 	</div> <!-- end our clients -->
 
 	<div class="our-partners section">
 		<div class="container">
-			<h2 class="section-title">Our Partners</h2>
+			<h2 class="section-title"><?php the_field( 'partners_title' ) ?></h2>
 			<div class="row">
+				<?php foreach ( get_field( 'partners' ) as $i => $p ): ?>
 				<div class="col-sm-6 col-md-4">
 					<div class="partners-img-wrap">
-						<div class="partners-img" style="background: url('/wp-content/themes/leadership/assets/images/partners-img.jpg')no-repeat center center;"></div>
+						<div class="partners-img" style="background: url('<?php echo $p['logo']; ?>')no-repeat center center;"></div>
 					</div>
-					<p>The Leadership Circle Profile is designed to accelerate leadership effectiveness beyond traditional competency-based approaches.</p>
+					<p><?php echo $p['descirption']; ?></p>
 				</div>
-
-				<div class="col-sm-6 col-md-4">
-					<div class="partners-img-wrap">
-						<div class="partners-img" style="background: url('/wp-content/themes/leadership/assets/images/partners-img.jpg')no-repeat center center;"></div>
-					</div>
-					<p>The Centre for International Business Coaching is a global coaching and consultancy company that specializes in customized support for individuals and companies faced with the complex challenges of working in the global economy.</p>
-				</div>
-
-				<div class="col-md-4">
-					<div class="partners-img-wrap">
-						<div class="partners-img" style="background: url('/wp-content/themes/leadership/assets/images/partners-img.jpg')no-repeat center center;"></div>
-					</div>
-					<p>The Leadership Circle Profile is designed to accelerate leadership effectiveness beyond traditional competency-based approaches.</p>
-				</div>
+				<?php endforeach; ?>
 			</div>
 		</div>
 	</div>
 
 	<div class="open-positions section">
 		<div class="container">
-			<h2 class="section-title">JOIN US ! OPEN POSITIONS </h2>
+			<h2 class="section-title"><?php the_field( 'join_us_title' ); ?></h2>
 
 			<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+				<?php foreach( get_field( 'jobs' ) as $i => $j ): ?>
 				<div class="panel panel-default">
 				    <div class="panel-heading" role="tab" id="heading-1">
 				      <h4 class="panel-title">
-				        <a class="" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse-1" aria-expanded="true" aria-controls="collapse1">
-				         	Job Title</a>
+				        <a class="" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse-<?php echo $i; ?>" aria-expanded="true" aria-controls="collapse-<?php echo $i; ?>"><?php echo $j['title']; ?></a>
 				      </h4>
 				    </div>
-				    <div id="collapse-1" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading-1" aria-expanded="true">
+				    <div id="collapse-<?php echo $i; ?>" class="panel-collapse collapse <?php echo $i == 0 ? 'in' : ''; ?>" role="tabpanel" aria-labelledby="heading-1" aria-expanded="<?php echo $i == 0 ? true : false; ?>">
 				      <div class="panel-body">
-				       	New interiors and adjustable seats create a luxurious space that is both warm and functional. The adaptable interior of the car is designed to carry your family, business and everything in between with ease and class.
+				       	<?php echo $j['description']; ?>
+
+				       	<p class="for-more-info">
+				       		for more info,<a href="mailto:<?php echo $j['email']; ?>"><?php echo $j['email']; ?></a>
+				       	</p>
 				      </div>
 				    </div>
 				</div>
-
-				<div class="panel panel-default">
-			    <div class="panel-heading" role="tab" id="heading-4">
-			      <h4 class="panel-title">
-			        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse-4" aria-expanded="false" aria-controls="collapse4">
-			         	Job Title</a>
-			      </h4>
-			    </div>
-				   <div id="collapse-4" class="panel-collapse collapse " role="tabpanel" aria-labelledby="heading-4" aria-expanded="false">
-				     <div class="panel-body">
-				       	The car’s central console was designed with your daily use in mind. A touchscreen audio system provides you with all the modern amenities to make your ride smooth and enjoyable.
-				     </div>
-				   </div>
-					</div>
+				<?php endforeach; ?>
 			</div>
 		</div>
 	</div>
